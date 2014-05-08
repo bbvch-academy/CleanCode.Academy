25e6a3bfc57f11dbc2eadeb0a06161d3a8019cf7
Marko Marković <markovic.marko@gmx.ch>
2014-05-08 11:19:46 +0200

    Tests: added Fody EnableFaking sample.

---
+++ source/Testing.Sample/2 Fody EnableFakting Sample/CalculatorTest.cs
@@ -0,0 +1,117 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CalculatorTest.cs" company="bbv Software Services AG">
+//   Copyright (c) 2014
+//   
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//   
+//   http://www.apache.org/licenses/LICENSE-2.0
+//   
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CleanCode.Testing.Sample
+{
+    using System.Security.AccessControl;
+
+    using CleanCode.Testing.Sample.Implementation;
+
+    using FakeItEasy;
+    using FluentAssertions;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CalculatorTest
+    {
+        private Calculator testee;
+
+        private AdditionOperator additionOperator;
+        private SubtractionOperator subtractionOperator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.additionOperator = A.Fake<AdditionOperator>();
+            this.subtractionOperator = A.Fake<SubtractionOperator>();
+
+            SetUpFakeAdditionOperator(this.additionOperator);
+            SetUpFakeSubtractionOperator(this.subtractionOperator);
+            
+            this.testee = new Calculator(this.additionOperator, this.subtractionOperator);
+        }
+
+        [Test]
+        public void CaluclatesSimpleAddition()
+        {
+            this.testee.SetNumber(23);
+            this.testee.PerformOperation(Calculator.AdditionOperation);
+            this.testee.SetNumber(17);
+            this.testee.PerformOperation(Calculator.EndOperation);
+
+            int result = this.testee.Result;
+
+            result.Should().Be(40, "we calculated 23+17");
+        }
+
+        [Test]
+        public void CaluclatesChainedAddition()
+        {
+            this.testee.SetNumber(23);
+            this.testee.PerformOperation(Calculator.AdditionOperation);
+            this.testee.SetNumber(17);
+            this.testee.PerformOperation(Calculator.AdditionOperation);
+            this.testee.SetNumber(15);
+            this.testee.PerformOperation(Calculator.AdditionOperation);
+            this.testee.SetNumber(20);
+            this.testee.PerformOperation(Calculator.EndOperation);
+
+            int result = this.testee.Result;
+
+            result.Should().Be(75, "we calculated 23+17+15+20");
+        }
+
+        [Test]
+        public void OverridesNumberInCalculation()
+        {
+            this.testee.SetNumber(23);
+            this.testee.PerformOperation(Calculator.AdditionOperation);
+            this.testee.SetNumber(17);
+            this.testee.SetNumber(20);
+            this.testee.PerformOperation(Calculator.EndOperation);
+
+            int result = this.testee.Result;
+
+            result.Should().Be(43, "we calculated 23+20");
+        }
+
+        [Test]
+        public void CaluclatesSimpleSubtraction()
+        {
+            this.testee.SetNumber(23);
+            this.testee.PerformOperation(Calculator.SubtractionOperation);
+            this.testee.SetNumber(17);
+            this.testee.PerformOperation(Calculator.EndOperation);
+
+            int result = this.testee.Result;
+
+            result.Should().Be(6, "we calculated 23-17");
+        }
+
+        private static void SetUpFakeAdditionOperator(AdditionOperator fakeOperator)
+        {
+            A.CallTo(() => fakeOperator.CalculateSum(A<int>._, A<int>._)).ReturnsLazily((int a, int b) => a + b);
+        }
+
+        private static void SetUpFakeSubtractionOperator(SubtractionOperator fakeOperator)
+        {
+            A.CallTo(() => fakeOperator.CalculateDifference(A<int>._, A<int>._)).ReturnsLazily((int a, int b) => a - b);
+        }
+    }
+}
---
+++ source/Testing.Sample/2 Fody EnableFakting Sample/Implementation/Calculator.cs
@@ -0,0 +1,74 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Calculator.cs" company="bbv Software Services AG">
+//   Copyright (c) 2014
+//   
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//   
+//   http://www.apache.org/licenses/LICENSE-2.0
+//   
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CleanCode.Testing.Sample.Implementation
+{
+    using System;
+
+    public class Calculator
+    {
+        public const string AdditionOperation = "Add";
+        public const string SubtractionOperation = "Sub";
+        public const string EndOperation = "End";
+        public const string NotDefinedOperation = "NDef";
+
+        private readonly AdditionOperator additionOperator;
+        private readonly SubtractionOperator subtractionOperator;
+
+        private int lastSetNumber;
+
+        private string lastSelectedOperation = NotDefinedOperation;
+
+        public Calculator(AdditionOperator additionOperator, SubtractionOperator subtractionOperator)
+        {
+            this.additionOperator = additionOperator;
+            this.subtractionOperator = subtractionOperator;
+        }
+
+        public int Result { get; private set; }
+
+        public void SetNumber(int value)
+        {
+            this.lastSetNumber = value;
+        }
+
+        public void PerformOperation(string operation)
+        {
+            this.Result = this.lastSelectedOperation != NotDefinedOperation
+                ? this.Calculate(this.lastSelectedOperation, this.Result, this.lastSetNumber)
+                : this.lastSetNumber;
+
+            this.lastSelectedOperation = operation;
+        }
+
+        private int Calculate(string operation, int firstNumber, int secondNumber)
+        {
+            switch (operation)
+            {
+                case AdditionOperation:
+                    return this.additionOperator.CalculateSum(firstNumber, secondNumber);
+
+                case SubtractionOperation:
+                    return this.subtractionOperator.CalculateDifference(firstNumber, secondNumber);
+
+                default:
+                    throw new ArgumentException("invalid operation " + operation);
+            }
+        }
+    }
+}
