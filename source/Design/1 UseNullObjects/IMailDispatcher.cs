ab51be46ac2b97b631e2b253da8afe6c2c152129
Marko Marković <markovic.marko@gmx.ch>
2014-05-12 22:42:48 +0200

    Desing: changed copyright tag to year 2014.

--- source/Design/1 UseNullObjects/IMailDispatcher.cs
+++ source/Design/1 UseNullObjects/IMailDispatcher.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="IMailDispatcher.cs" company="bbv Software Services AG">
-//   Copyright (c) 2013
+//   Copyright (c) 2014
 //   
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
